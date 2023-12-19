a5fd59f26818c5e3a05099078b0ffcaeae31520e
Burhan Ali Shaikh <85751219+Nyx-95@users.noreply.github.com>
2023-12-19 15:52:44 +0500

    update

--- fmdFolder/fmdDoctor/src/Main/WebPage/login.jsx
+++ fmdFolder/fmdDoctor/src/Main/WebPage/login.jsx
@@ -88,7 +88,6 @@ export const Login = () => {
                                         Sign In
                                     </button>
                                 </div>
-
                             </form>
                         </div>
                     </div>
